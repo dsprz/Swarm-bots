b5d639063d518cda4601fef78169fddd7c99db42
Koralie Porcel <94242255+Ilarak@users.noreply.github.com>
2022-11-28 18:47:24 +0100

    Update Traducteur.h

--- PORCEL Koralie/c++/Traducteur.h
+++ PORCEL Koralie/c++/Traducteur.h
@@ -2,48 +2,52 @@
 
 class Traducteur {
     public :
-        Traducteur();
-        // les deux fonctions permettant de traduire :
-        void traduitDelay(String mot);    // en utilisant delay
-        void traduitMillis(String mot);   // en utilisant millis
+      Traducteur();
+      void traduitMot(String mot); // Fonction qui permet de traduire
     private : 
-      const char court[2]="c";            //correspond à un temps court
+      void lettreTrouve(int i);
+      bool allumerLed(bool ledState, int duree);
+      bool attendre(int duree);
+      const char court[2]="s";            //correspond à un temps short donc court
       unsigned long currentTime=0;        //permet d'utiliser la fonction millis dans le Traducteur.cpp
       unsigned long previousTime=0;
-      char charLettre;                    // utile dans le Traducteur.cpp pour pour transformer une lettre d'un tableau en char
-      void allumerLedDelay(int duree);
-      bool allumerLedMillis(int duree, bool ledState);
-      bool AttendreMillis(int duree);
-      String TabTraductlettres[28][2]={   //on met dans une matrice à deux colonnes la lettre de l'alphabet (colonne 1) et sa traduction en morse (colonne 2)
-         {"a","cl"},
-         {"b","lccc"},
-         {"c","lclc"},
-         {"d","lcc"},
-         {"e","c"},
-         {"f","cclc"},
-         {"g","llc"},
-         {"h","cccc"},
-         {"i","cc"},
-         {"j","clll"},
-         {"k","lcl"},
-         {"l","clcc"},
+      String TabTraductlettres[37][2]={   //on met dans une matrice à deux colonnes la lettre de l'alphabet (colonne 1) et sa traduction en morse (colonne 2)
+         {"0","lllll"},
+         {"1","sllll"},
+         {"2","sslll"},
+         {"3","sssll"},
+         {"4","ssssl"},
+         {"5","sssss"},
+         {"6","lssss"},
+         {"7","llsss"},
+         {"8","lllss"},
+         {"9","lllls"},
+         {"a","sl"},
+         {"b","lsss"},
+         {"c","lsls"},
+         {"d","lss"},
+         {"e","s"},
+         {"f","ssls"},
+         {"g","lls"},
+         {"h","ssss"},
+         {"i","ss"},
+         {"j","slll"},
+         {"k","lsl"},
+         {"l","slss"},
          {"m","ll"},
-         {"n","lc"},
+         {"n","ls"},
          {"o","lll"},
-         {"p","cllc"},
-         {"q","llcl"},
-         {"r","clc"},
-         {"s","ccc"},
+         {"p","slls"},
+         {"q","llsl"},
+         {"r","sls"},
+         {"s","sss"},
          {"t","l"},
-         {"u","ccl"},
-         {"v","cccl"},
-         {"w","cll"},
-         {"x","lccl"},
-         {"y","lcll"},
-         {"z","llcc"},
+         {"u","ssl"},
+         {"v","sssl"},
+         {"w","sll"},
+         {"x","lssl"},
+         {"y","lsll"},
+         {"z","llss"},
          {" ",""},
-         {"",""},     // on rajoute ce cas pour verrifier si ce n'est pas une lettre (utilisé dans le Traducteur.cpp)
          };
    };
- 
- //+1
