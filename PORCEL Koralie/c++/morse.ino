518f85f759d0a64d33f4f25f8f4670aea71fe864
Koralie Porcel <94242255+Ilarak@users.noreply.github.com>
2022-11-28 18:47:42 +0100

    Update morse.ino

--- PORCEL Koralie/c++/morse.ino
+++ PORCEL Koralie/c++/morse.ino
@@ -1,7 +1,5 @@
 #include "Traducteur.h" 
-Traducteur traducteur = Traducteur();
-String motTraduire;
-String delayOuMillis;
+Traducteur traducteur = Traducteur();  //crée un objet de la classe traducteur
 
 void setup() {
   Serial.begin(9600);
@@ -9,24 +7,10 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Quel mot voulez vous traduire en morse ? ");
-  while(Serial.available() ==0) {}       //ca permet de mettre en pause le terminal et d'attendre d'avoir une réponse pour le mettre dans motTraduire
-  motTraduire = Serial.readString();
-  delayOuMillis="";
-  Serial.println("En utilisant delay (d) ou millis (m) ?");
-  while(Serial.available() == 0) {}
-  delayOuMillis = Serial.readString();
-  while((delayOuMillis!="d") && (delayOuMillis!="m")){          // si la personne n'a pas répondu d ou m, rentre dans une boucle tant qu'ils écrivent pas l'un des deux
-    Serial.println("Non, vous devez écrire 'd' pour delay ou 'm' pour millis");
-    while(Serial.available() == 0) {}
-    delayOuMillis = Serial.readString();
-    }
-  Serial.print("Le mot choisi est : ");
-  Serial.println(motTraduire);
-  if (delayOuMillis=="d"){
-    traducteur.traduitDelay(motTraduire);
-    }
-  else{
-    traducteur.traduitMillis(motTraduire);
-    }
+  Serial.println("Quel est la phrase que vous voulez traduire en morse ? ");
+  while(Serial.available() == 0) {}       //ca permet de mettre en pause le terminal et d'attendre d'avoir une réponse pour le mettre dans motATraduire
+  String motATraduire = Serial.readString();
+  Serial.print("Vous avez choisi comme phrase : ");
+  Serial.println(motATraduire);
+  traducteur.traduitMot(motATraduire);
 }
