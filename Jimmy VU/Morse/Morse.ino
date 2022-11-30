6140c6d127578e1110a6483621622a0d859de85b
dsprz <84564775+dsprz@users.noreply.github.com>
2022-11-30 21:25:48 +0100

    Update Morse.ino

--- Jimmy VU/Morse/Morse.ino
+++ Jimmy VU/Morse/Morse.ino
@@ -1,3 +1,11 @@
+/* Ecrit par Jimmy VU
+ * Pour vos tests, vous pouvez modifier le temps 
+ * durant lequel la LED est allumée/éteinte
+ * en allant dans LettreMorse.cpp et en modifiant
+ * la variable const int interval qui se trouve
+ * en tête du fichier. Par défaut elle vaut 1000.
+ */
+
 #include "LettreMorse.h"
 #include "Traducteur.h"
 
@@ -11,6 +19,8 @@ void setup()
 
 void loop()
 {
-    //translator.translateWord("Ecrivez une phrase ici");
+    /*Utilisez translator.translateWord(string) pour traduire une phrase.
+     *translator.translateWord("Ecrivez une phrase ici");
+     */
     translator.translateWord("é'A,RbR@e!;");
 }
